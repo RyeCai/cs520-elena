891955a9886f12776c09b7ef2e54df042ab0b76e
Eric Stevens <uspeople@icloud.com>
2023-05-16 13:07:49 -0400

    Added OSM api call to return the ways using overpass

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import L from "../node_modules/leaflet/dist/leaflet.js"; // // Creating map options
+import axios from "../node_modules/axios/dist/axios.js";
 function App() {
   const [mapResult, setmapResult] = useState();
   const [searchBox, setSearchText] = useState({
-    minLon: 42.3644,
-    minLat: -72.5531,
-    maxLon: 42.373,
-    maxLat: -72.536,
+    minLon: -72.545899,
+    minLat: 42.350808,
+    maxLon: -72.5,
+    maxLat: 42.361899,
   });
+
   const [tempText, setTempText] = useState();
   const layer = new L.TileLayer(
     "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -38,32 +40,79 @@ function App() {
     getMaps();
     return;
   }, [searchBox]);
-  console.log(mapResult);
-    useEffect(() => {
-      async function renderMap() {
-        console.log("rendering");
-        let mapCenter = [17.385044, 78.486671];
-        if (mapResult != undefined) {
-          let m1 = parseFloat(mapResult[0]) + parseFloat(mapResult[1]);
-          let m2 = parseFloat(mapResult[2]) + parseFloat(mapResult[3]);
-          mapCenter[1] = m2 / 2;
-          mapCenter[0] = m1 / 2;
-          mapCenter = [17.385044, 78.486671];
-        }
-        var mapOptions = {
-          center: mapCenter,
-          zoom: 10,
-        };
-        // // Creating a map object
-        var map = (new L.map("map", mapOptions));
-        // // Creating a Layer object
-        // // Adding layer to the map
-        map.addLayer(layer);
-      }
-      renderMap();
-      return;
-    }, []);
 
+  // Define the Overpass API query currently set to amherst 
+  const minLon = -72.545899;
+  const minLat = 42.350808;
+  const maxLon = -72.5;
+  const maxLat = 42.361899;
+
+  // Define the Overpass API query with the bounding box
+  const overpassQuery = `
+  [out:json];
+  way(${minLat},${minLon},${maxLat},${maxLon});
+  out geom;
+`;
+  // Create a function to fetch ways using the Overpass API
+  async function fetchWays() {
+    try {
+      // Make the request to the Overpass API
+      const response = await axios.get(
+        "https://overpass-api.de/api/interpreter",
+        {
+          params: {
+            data: overpassQuery,
+          },
+        }
+      );
+
+      // Extract the ways from the response data
+      const ways = response.data.elements.filter(
+        (element) => element.type === "way"
+      );
+
+      // Process the ways as needed
+      return ways;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  // Call the fetchWays function to retrieve the ways
+  // returns a list of OSM ways which are composed of lat/long points that define the road
+  let ways;
+  if (mapResult != undefined) {
+    const getWays = async () => {
+      ways = await fetchWays();
+      console.log(ways);
+    };
+    getWays();
+  }
+
+  useEffect(() => {
+    async function renderMap() {
+      console.log("rendering");
+      let mapCenter = [17.385044, 78.486671];
+      if (mapResult != undefined) {
+        let m1 = parseFloat(mapResult[0]) + parseFloat(mapResult[1]);
+        let m2 = parseFloat(mapResult[2]) + parseFloat(mapResult[3]);
+        mapCenter[1] = m2 / 2;
+        mapCenter[0] = m1 / 2;
+        mapCenter = [17.385044, 78.486671];
+      }
+      var mapOptions = {
+        center: mapCenter,
+        zoom: 10,
+      };
+      // // Creating a map object
+      var map = new L.map("map", mapOptions);
+      // // Creating a Layer object
+      // // Adding layer to the map
+      map.addLayer(layer);
+    }
+    renderMap();
+    return;
+  }, []);
 
   const [MinMax, setMinMax] = useState("Minimize");
 
@@ -75,7 +124,7 @@ function App() {
   };
 
   const startSearch = (e) => {
-    e
+    e;
     setSearchText(tempText);
   };
 
