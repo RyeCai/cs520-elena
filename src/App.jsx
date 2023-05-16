34057970b4c886718e48ee3098560c7ba7b58956
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-05-16 13:31:36 -0400

    chore: use umass coords

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "leaflet/dist/leaflet.css";
+import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "./App.css";
 
+const center = [42.3906, -72.5283];
+const zoom = 16;
+
 function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={center} zoom={zoom} scrollWheelZoom={true}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     </MapContainer>
   );
