da0c7cd0f23171b4b1ef44420a8123301c23e36d
Eric Stevens <uspeople@icloud.com>
2023-05-15 21:43:40 -0400

    Added a call to the openmaps
    Added a call to the openmaps API and arrange the map to view the center of "England" for now, will add a search bar next

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
 import './App.css'
 import L from  '../node_modules/leaflet/dist/leaflet.js'   // // Creating map options
 
 function App() {
-  const [count, setCount] = useState(0)
-    var mapOptions = {
-      center: [17.385044, 78.486671],
+    const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+    const [mapResult, setmapResult] = useState();
+    const [searchText, setSearchText] = useState("England");
+      
+useEffect(() => {
+    async function getMaps() {
+    const params = {
+                q: searchText,
+                format: "json",
+                addressdetails: 1,
+                polygon_geojson: 0,
+              };
+     const queryString = new URLSearchParams(params).toString();
+              const requestOptions = {
+                method: "GET",
+                redirect: "follow",
+              };
+              fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+                .then((response) => response.text())
+                .then((result) => {
+                  setmapResult(JSON.parse(result)[0].boundingbox);
+                  if(map != undefined){ 
+                    map.remove();
+                    }
+                })
+                .catch((err) => console.log("err: ", err));
+              }
+              getMaps();
+          return;
+  }, []);
+
+  console.log(mapResult)
+  let mapCenter = [17.385044, 78.486671];
+  if(mapResult != undefined){
+    let m1 = parseFloat(mapResult[0]) + parseFloat(mapResult[1])
+    let m2 = parseFloat(mapResult[2]) + parseFloat(mapResult[3])
+    mapCenter[1] = m2 / 2
+    mapCenter[0] = m1 / 2
+  }
+  var mapOptions = {
+      center: mapCenter,
       zoom: 10
     }
     // // Creating a map object
+    
+    if(map != undefined){
+      map.remove();
+    }
+    
     var map = new L.map('map', mapOptions);
-    // console.log(map);
     // // Creating a Layer object
     var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
     // // Adding layer to the map
     map.addLayer(layer);
-    // const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
-    // let searchText = "England"
-    // const params = {
-    //             q: searchText,
-    //             format: "json",
-    //             addressdetails: 1,
-    //             polygon_geojson: 0,
-    //           };
-    //  const queryString = new URLSearchParams(params).toString();
-    //           const requestOptions = {
-    //             method: "GET",
-    //             redirect: "follow",
-    //           };
-    //           fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
-    //             .then((response) => response.text())
-    //             .then((result) => {
-    //               console.log(JSON.parse(result));
-    //             })
-    //             .catch((err) => console.log("err: ", err));
     
 const [MinMax, setMinMax] = useState("Minimize")
 
@@ -48,7 +68,6 @@ const onOptionChange = e => {
 
   return (
     <>
-       This is a test my guy!
       <input
         type="radio"
         name="topping"
