113d111fa48293dcf357776b42cd47d46dbb08fe
Eric Stevens <uspeople@icloud.com>
2023-05-15 20:36:12 -0400

    Moved main app to correct file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import L from  '../node_modules/leaflet/dist/leaflet.js'   // // Creating map options
 
 function App() {
   const [count, setCount] = useState(0)
+
+
+
+    var mapOptions = {
+      center: [17.385044, 78.486671],
+      zoom: 10
+    }
+    // // Creating a map object
+    var map = new L.map('map', mapOptions);
+    // console.log(map);
+    // // Creating a Layer object
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    // // Adding layer to the map
+    map.addLayer(layer);
+
+
+    
+    
+    // const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+    // let searchText = "England"
+    // const params = {
+    //             q: searchText,
+    //             format: "json",
+    //             addressdetails: 1,
+    //             polygon_geojson: 0,
+    //           };
+    //  const queryString = new URLSearchParams(params).toString();
+    //           const requestOptions = {
+    //             method: "GET",
+    //             redirect: "follow",
+    //           };
+    //           fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+    //             .then((response) => response.text())
+    //             .then((result) => {
+    //               console.log(JSON.parse(result));
+    //             })
+    //             .catch((err) => console.log("err: ", err));
+    
+const [topping, setTopping] = useState("Medium")
+
+
+
 
   return (
     <>
@@ -28,6 +72,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       This is a test my guy!
+      <div>
+        <input type="radio" value="Maximize" name = "minMax" /> Maximize
+        <input type="radio" value="Minimize" name = "minMax" /> Minimize
+      </div>
     </>
   )
 }
