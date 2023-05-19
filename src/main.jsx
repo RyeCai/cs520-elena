f2e3b400471cdac061b6ee3661353651ce01c781
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-05-19 10:24:15 -0400

    fix: no mui/styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import { createTheme } from "@mui/material";
-import { ThemeProvider } from "@mui/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
