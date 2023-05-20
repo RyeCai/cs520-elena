5a48c9daf3febdc2a73fa0523aedb485c75b002a
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-05-20 16:29:30 -0400

    refactor: remove overlay logic

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,33 +1,18 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import { match } from "ts-pattern";
-import { InputContext } from "../App.jsx";
 
 const UMASS_LOCATION = [42.3906, -72.5283];
 const DEFAULT_ZOOM = 16;
 
 function MapController() {
   const map = useMap();
-  const { destinationLocation, setOverlayContent } = useContext(InputContext);
   const [userLocation, setUserLocation] = useState(undefined);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => setUserLocation([position.coords.latitude, position.coords.longitude]),
-      error =>
-        match(error.code)
-          .with(error.PERMISSION_DENIED, () =>
-            setOverlayContent(
-              "This application requires location information. Please enable location permissions in your browsers settings."
-            )
-          )
-          .with(error.POSITION_UNAVAILABLE, () =>
-            setOverlayContent("Unable to retrieve location information. Please refresh and try again.")
-          )
-          .with(error.TIMEOUT, () => setOverlayContent("Timed out. Please refresh and try again."))
-          .otherwise(() => setOverlayContent("An unknown error occurred. Please refresh and try again."))
+    navigator.geolocation.getCurrentPosition(position =>
+      setUserLocation([position.coords.latitude, position.coords.longitude])
     );
-  }, [setUserLocation, setOverlayContent]);
+  }, [setUserLocation]);
 
   useEffect(() => {
     if (userLocation) map.flyTo(userLocation, DEFAULT_ZOOM);
