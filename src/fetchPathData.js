76512ab59cb81803a2dd912f44ad27c61e67d228
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-05-21 22:07:26 -0400

    fix: random elevation data in debug mode

--- src/fetchPathData.js
+++ src/fetchPathData.js
@@ -1,6 +1,8 @@
 import Bottleneck from "bottleneck";
-import { getBounds } from "geolib";
+import { getBounds, getDistance } from "geolib";
 import wretch from "wretch";
+
+const DEBUG = true;
 
 function makePoint(latitude, longitude) {
   return { latitude, longitude };
@@ -26,7 +28,7 @@ const limiter = new Bottleneck({
   minTime: 333,
 });
 
-const makeEleKey = ({ lon, lat }) => `[${lon.toFixed(6)},${lat.toFixed(6)}]`;
+const makeEleKey = (lat, lon) => `[${lat.toFixed(6)},${lon.toFixed(6)}]`;
 
 async function fetchElevationData({ elements }) {
   const elevationMap = {};
@@ -48,7 +50,7 @@ async function fetchElevationData({ elements }) {
   for (const query of elevationQueries) {
     const { results } = await limiter.schedule(() => fetchElevations(query));
     for (const { longitude, latitude, elevation } of results) {
-      const key = makeEleKey({ lon: longitude, lat: latitude });
+      const key = makeEleKey(latitude, longitude);
 
       elevationMap[key] = elevation;
     }
@@ -72,25 +74,43 @@ export default async function fetchPathData(startCoords, endCoords) {
     out;`;
 
   const osmResult = await fetchOSMData(query);
-  const elevationMap = await fetchElevationData(osmResult);
+  const elevationMap = DEBUG ? {} : await fetchElevationData(osmResult);
 
   const nodes = {},
     ways = {};
 
+  let startNodeId, endNodeId;
+  let bestStartDist = Infinity,
+    bestEndDist = Infinity;
+
   for (const e of osmResult.elements) {
     if (e.type === "node") {
       const { lat, lon } = e;
-      const eleKey = makeEleKey({ lat, lon });
+      const eleKey = makeEleKey(lat, lon);
+
+      const currPoint = makePoint(lat, lon);
+
+      const distToStart = getDistance(startPoint, currPoint);
+      if (distToStart < bestStartDist) {
+        bestStartDist = distToStart;
+        startNodeId = e.id;
+      }
+
+      const distToEnd = getDistance(endPoint, currPoint);
+      if (distToEnd < bestEndDist) {
+        bestEndDist = distToEnd;
+        endNodeId = e.id;
+      }
 
       nodes[e.id] = {
         lat: e.lat,
         lon: e.lon,
-        ele: elevationMap[eleKey],
+        ele: DEBUG ? Math.floor(Math.random() * 20 + 30) : elevationMap[eleKey],
       };
     } else if (e.type === "way") {
       ways[e.id] = e.nodes;
     }
   }
 
-  return [nodes, ways];
+  return { startNodeId, endNodeId, nodes, ways };
 }
--- src/findPath.js
+++ src/findPath.js
@@ -1,8 +1,10 @@
 import fetchPathData from "./fetchPathData.js";
 
 export default async function findPath({ startCoords, endCoords, elevationGain, extraDistance }) {
-  const [nodes, ways] = await fetchPathData(startCoords, endCoords);
+  const { startNodeId, endNodeId, nodes, ways } = await fetchPathData(startCoords, endCoords);
 
+  console.log(nodes[startNodeId]);
+  console.log(nodes[endNodeId]);
   console.log(nodes);
   console.log(ways);
 }
